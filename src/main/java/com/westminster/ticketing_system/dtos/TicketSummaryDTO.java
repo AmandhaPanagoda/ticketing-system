6f7d744d3365d8aa05bcba2d27caa90d80f34839
AmandhaPanagoda <amandhaisiri@gmail.com>
2024-12-01 01:48:05 +0530

    comments

--- src/main/java/com/westminster/ticketing_system/dtos/TicketSummaryDTO.java
+++ src/main/java/com/westminster/ticketing_system/dtos/TicketSummaryDTO.java
@@ -5,6 +5,12 @@ import lombok.Data;
 
 /**
  * DTO for transferring summary information about tickets between layers.
+ * This was created forthe version 1 where users can add multiple tickets for
+ * mulitple events.
+ * Now, it is used for version 2 where users can add a single ticket for a
+ * single event.
+ * Many of the fields are not used in version 2, but they are kept for future
+ * use.
  */
 @Data
 public class TicketSummaryDTO {
